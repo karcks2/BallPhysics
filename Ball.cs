3df97d10ae91c701ce5d63a3210b273fb7ab9ce1
kaarel karp <kaarelkaarel39@gmail.com>
2024-01-27 14:08:05 +0200

    Add project files.

---
+++ Ball.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BallPhysicsEngine
+{
+    internal class Ball : PictureBox
+    {
+        public int horVelocity = 1;
+        public int vertVelocity = 1;
+
+        public Ball()
+        {
+            this.Width = 20;
+            this.Height = 20;
+            this.BackColor = Color.White;
+
+        }
+
+    }
+}
