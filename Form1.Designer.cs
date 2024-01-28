5c88158d7bf40287db7378e5ee7845e618777dc9
kaarel karp <kaarelkaarel39@gmail.com>
2024-01-28 18:28:21 +0200

    PLayer 1 and Player 2 added needs the player movment everything else is starting to come together

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,30 +29,56 @@
         private void InitializeComponent()
         {
             Ball = new PictureBox();
+            player2 = new PictureBox();
+            player1 = new PictureBox();
             ((System.ComponentModel.ISupportInitialize)Ball).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)player2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)player1).BeginInit();
             SuspendLayout();
             // 
             // Ball
             // 
-            Ball.Location = new Point(353, 198);
+            Ball.Location = new Point(367, 195);
             Ball.Name = "Ball";
             Ball.Size = new Size(38, 40);
             Ball.TabIndex = 0;
             Ball.TabStop = false;
+            // 
+            // player2
+            // 
+            player2.Location = new Point(750, 138);
+            player2.Name = "player2";
+            player2.Size = new Size(38, 191);
+            player2.TabIndex = 1;
+            player2.TabStop = false;
+            // 
+            // player1
+            // 
+            player1.Location = new Point(12, 138);
+            player1.Name = "player1";
+            player1.Size = new Size(38, 172);
+            player1.TabIndex = 2;
+            player1.TabStop = false;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(12F, 30F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(player1);
+            Controls.Add(player2);
             Controls.Add(Ball);
             Name = "Form1";
             ((System.ComponentModel.ISupportInitialize)Ball).EndInit();
+            ((System.ComponentModel.ISupportInitialize)player2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)player1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
         private PictureBox Ball;
+        private PictureBox player2;
+        private PictureBox player1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,6 @@
+using System.IO;
+using System.Windows.Forms;
+
 namespace BallPysichs
 {
     public partial class Form1 : Form
@@ -5,13 +8,17 @@ namespace BallPysichs
         System.Windows.Forms.Timer mainTimer = null;
 
         private int horVelocity = 2;
-        private int verVelocity = 2;
+        private int verVelocity = 0;
 
         public Form1()
         {
             InitializeComponent();
-            InitializeGame();
             InitalizeMainTimer();
+
+            InitializeGame();
+            InitalizeBall();  
+            InitalizePlayer();  
+            
         }
 
         public void InitializeGame()
@@ -25,6 +32,10 @@ namespace BallPysichs
 
             this.Text = "Bouncing ball";
 
+        }
+
+        public void InitalizeBall()
+        {
 
             Ball.Width = 20;
             Ball.Height = 20;
@@ -33,6 +44,20 @@ namespace BallPysichs
             Ball.Top = 190;
 
             Ball.BackColor = Color.White;
+        }
+
+        public void InitalizePlayer()
+        {
+            player1.BackColor = Color.White;
+            player2.BackColor = Color.White;
+
+            player1.Width = 20;
+            player1.Height = 100;
+
+            player2.Width = 20;
+            player2.Height = 100;
+
+
         }
 
         private void InitalizeMainTimer()
@@ -49,6 +74,8 @@ namespace BallPysichs
             Ball.Top += verVelocity;
 
             BallBorderCollision();
+            BallPlayer1Collison();
+            BallPlayer2Collison();
         }
 
         private void BallBorderCollision()
@@ -56,17 +83,72 @@ namespace BallPysichs
 
             if (Ball.Left <= 0 || Ball.Left + Ball.Width >= ClientRectangle.Width)
             {
-                horVelocity *= -1;
-                horVelocity += 2;
-
+                mainTimer.Stop();
+                MessageBox.Show("Game over press space to start again");
+                SetBallLocationToStart();
+                verVelocity = 0;
+                horVelocity = 2;
+                mainTimer.Start();
             }
-            else if (Ball.Right <= 0 || Ball.Right + Ball.Width >= ClientRectangle.Width)
+            else if (Ball.Top <= 0 || Ball.Top + Ball.Height >= ClientRectangle.Height)
             {
+                verVelocity *= -1;
+            }
+        }
+
+        private void SetBallLocationToStart()
+        {
+            int x = 190;
+            int y = 190;
+
+            Point newLocation = new Point(x, y);
+
+            Ball.Location = newLocation;
+        }
+
+        private void BallPlayer2Collison()
+        {
+            if (Ball.Bounds.IntersectsWith(player2.Bounds))
+            {
+                // Change direction when collision occurs
                 horVelocity *= -1;
+                verVelocity *= -1;
+                verVelocity += 0;
                 horVelocity += -2;
             }
 
-            else if (Ball.Top <= 0 || Ball.Top + Ball.Height >= ClientRectangle.Height)
+            // Add boundary checking logic if needed
+            // Example: Reverse direction when hitting form boundaries
+            if (Ball.Left <= 0 || player2.Right >= ClientSize.Width)
+            {
+                horVelocity *= -1;
+            }
+
+            if (Ball.Top <= 0 || player2.Bottom >= ClientSize.Height)
+            {
+                verVelocity *= -1;
+            }
+        }
+
+        private void BallPlayer1Collison()
+        {
+            if (Ball.Bounds.IntersectsWith(player1.Bounds))
+            {
+                // Change direction when collision occurs
+                horVelocity *= -1;
+                verVelocity *= -1;
+                verVelocity += 0;
+                horVelocity += 2;
+            }
+
+            // Add boundary checking logic if needed
+            // Example: Reverse direction when hitting form boundaries
+            if (Ball.Left <= 0 || player1.Right >= ClientSize.Width)
+            {
+                horVelocity += -5;
+            }
+
+            if (Ball.Top <= 0 || player1.Bottom >= ClientSize.Height)
             {
                 verVelocity *= -1;
             }
